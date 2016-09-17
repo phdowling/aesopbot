9d940c82e3ffcc8d90e0ea912d92448a2532ec24
Philipp Dowling <phdowling@users.noreply.github.com>
2016-09-18 01:30:53 +0200

    kjasdng

--- generator.py
+++ generator.py
@@ -101,7 +101,7 @@ def run():
             #print u"sending %s (%s)" % (seq, source)
             try:
                 res = requests.post(URL, json={u"lyrics": seq, u"isGenerated": source == u"GEN"})
-                time.sleep(0.1)
+                time.sleep(0.05)
             except:
                 print "Error sending!"
                 time.sleep(3)
