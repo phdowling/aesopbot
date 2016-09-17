72690a4109431878e43107d295c868be799e7c74
Philipp Dowling <phdowling@users.noreply.github.com>
2016-09-17 23:02:01 +0200

    skjdbnf

--- node/aesopbot/routes/index.js
+++ node/aesopbot/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("Got connection!");
-  socket.emit("general", "Hello!");
+  res.io.emit("general", "Hello!");
   res.render('index', { title: 'Express' });
 });
 
