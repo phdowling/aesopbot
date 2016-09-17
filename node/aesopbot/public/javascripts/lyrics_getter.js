b43a85a981e2dddd4f663f32a6c886a9f6b5ef57
Philipp Dowling <phdowling@users.noreply.github.com>
2016-09-17 22:57:43 +0200

    skjdbnf

--- node/aesopbot/public/javascripts/lyrics_getter.js
+++ node/aesopbot/public/javascripts/lyrics_getter.js
@@ -2,7 +2,7 @@
  * Created by dowling on 17/09/16.
  */
 
-var socket = io('http://localhost:3000');
+var socket = io('http://localhost');
 console.log("TEST");
 socket.on("general", function (data) {
     console.log(data);
