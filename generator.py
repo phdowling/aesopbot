f20ee2eb878f0dd02207cb6e7c900d5b92c23a4f
Philipp Dowling <phdowling@users.noreply.github.com>
2016-09-17 21:37:12 +0200

    skjdfn

--- generator.py
+++ generator.py
@@ -1,17 +1,16 @@
-from __future__ import print_function
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.layers import LSTM
-from keras.optimizers import RMSprop
-from keras.utils.data_utils import get_file
-import numpy as np
+import keras
 import random
+import numpy as np
 import sys
+
 
 text = u""
 for line in open("all_lyrics_kanye.txt"):
     if "google" not in line.lower():
         text += line.decode("utf-8")
+
+model = keras.models.model_from_json("trained_kanye.model")
+model.load_weights("trained_kanye.model.weights")
 
 print('corpus length:', len(text))
 
@@ -38,18 +37,6 @@ for i, sentence in enumerate(sentences):
         X[i, t, char_indices[char]] = 1
     y[i, char_indices[next_chars[i]]] = 1
 
-
-# build the model: a single LSTM
-print('Build model...')
-model = Sequential()
-model.add(LSTM(128, input_shape=(maxlen, len(chars))))
-model.add(Dense(len(chars)))
-model.add(Activation('softmax'))
-
-optimizer = RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer)
-
-
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
     preds = np.asarray(preds).astype('float64')
@@ -59,37 +46,34 @@ def sample(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
-# train the model, output generated text after each iteration
-for iteration in range(1, 60):
-    print()
-    print('-' * 50)
-    print('Iteration', iteration)
-    model.fit(X, y, batch_size=128, nb_epoch=1)
 
+def generate_bars():
     start_index = random.randint(0, len(text) - maxlen - 1)
-
     for diversity in [0.2, 0.5, 1.0, 1.2]:
         print()
         print('----- diversity:', diversity)
-
         generated = ''
         sentence = text[start_index: start_index + maxlen]
+        yield sentence, "SEED"
         generated += sentence
         print('----- Generating with seed: "' + sentence + '"')
         sys.stdout.write(generated)
-
         for i in range(400):
             x = np.zeros((1, maxlen, len(chars)))
             for t, char in enumerate(sentence):
                 x[0, t, char_indices[char]] = 1.
-
             preds = model.predict(x, verbose=0)[0]
             next_index = sample(preds, diversity)
             next_char = indices_char[next_index]
-
             generated += next_char
             sentence = sentence[1:] + next_char
 
             sys.stdout.write(next_char)
             sys.stdout.flush()
+
+            yield next_char, "GEN"
         print()
+
+def run():
+    while True:
+        for seq, source in generate_bars():
