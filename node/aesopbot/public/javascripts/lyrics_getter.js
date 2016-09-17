f9c636998734cfe0285f78447298639168bd9ba9
Philipp Dowling <phdowling@users.noreply.github.com>
2016-09-18 01:27:28 +0200

    kjasdng

--- node/aesopbot/public/javascripts/lyrics_getter.js
+++ node/aesopbot/public/javascripts/lyrics_getter.js
@@ -10,15 +10,15 @@ socket.on("general", function (data) {
 
 socket.on("lyrics", function (data) {
     //console.log("GEN: " + JSON.stringify(data));
-    var htmlIzed = "<span class='seed'>" + data.replace(/(?:\r\n|\r|\n)/g, '<br />') + "</span>";
+    var htmlIzed = data.replace(/(?:\r\n|\r|\n)/g, '<br />');
     var container = $(".container");
     container.html(container.html() + htmlIzed);
 });
 
 socket.on("seed", function (data) {
     console.log("SEED: " + JSON.stringify(data));
-    var htmlIzed = data.replace(/(?:\r\n|\r|\n)/g, '<br />');
 
+    var htmlIzed = "<span class='seed'>" + data.replace(/(?:\r\n|\r|\n)/g, '<br />') + "</span>";
     var container = $(".container");
     container.empty();
     container.html(htmlIzed);
